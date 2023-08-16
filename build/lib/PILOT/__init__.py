c45df8c2a37269d6378be870a6f3d60450c80ead
mjoodaki <mjoodaki@ukaachen.de>
2023-08-16 10:34:48 +0200

    delete built fo

--- build/lib/PILOT/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "1.1.0"
-__author__ = 'Mehdi Joodaki'
-__credits__ = 'Institute for Computational Genomics'
-
-from . import tl
-from . import pl
--- build/lib/PILOT/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Trajectory import *
-from .Cell_gene_selection import *
-from .Gene_cluster_specific import *
-from .patients_sub_clustering import *
